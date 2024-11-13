0ae4e5a2b2caac3679d8f2658485916fc27e4408
czanescu <cristian.zanescu@s.unibuc.ro>
2024-11-13 15:22:49 +0200

    tradus in limba romana

--- if.sh
+++ if.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 if [ ! -e "$1" ]; then
-	echo "$1 does not exist"
+	echo "$1 nu exista"
 elif [ -f "$1" ]; then
-	echo "$1 is a regular file"
+	echo "$1 este un fisier normal"
 else
-	echo "$1 exists, but we don't know what kind of file it is"
-fi
+	echo "$1 exista, insa nu stim nimic despre el"
